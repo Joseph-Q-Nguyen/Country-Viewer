51262d74f12558ad715fbe38170ee535a28dd80b
Joseph Nguyen <joseph.nguyenor@gmail.com>
2024-10-21 15:08:22 -0700

    Update to Countries

--- client/src/App.js
+++ client/src/App.js
@@ -1,72 +1,78 @@
 import React from "react";
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-import Home from './components/Home';
-import Country from './components/Country';
-import Navbar from 'react-bootstrap/Navbar';
-import Container from 'react-bootstrap/Container';
-import { DEPLOYMENT_URL } from './Constants';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
+import Country from "./components/Country";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import { DEPLOYMENT_URL } from "./Constants";
 
 function App() {
-  const src = require('./globe.png');
+  const src = require("./globe.png");
   const [searched, setSearched] = React.useState(false);
   const [value, setValue] = React.useState(null);
   const [region, setRegion] = React.useState(null);
-  
+
   React.useEffect(() => {
     if (searched) {
-      window.location.href = DEPLOYMENT_URL + "/?search=" + value 
-      + "&region=" + region;
+      window.location.href =
+        DEPLOYMENT_URL + "/?search=" + value + "&region=" + region;
     }
-    
   }, [searched]);
 
   return (
     <div className="App">
-      <Navbar className='nav-bar' variant='light' sticky="top">
+      <Navbar className="nav-bar" variant="light" sticky="top">
         <Container>
           <Navbar.Brand href="/">
             <img
               alt=""
-              src={ src }
+              src={src}
               width="30"
               height="30"
               className="d-inline-block align-top"
-            />{' '}
-          Country Viewer
+            />{" "}
+            Countries
           </Navbar.Brand>
-          <form onSubmit={() => {setSearched(true)}}>
-              <label>Search for Countries:</label>
-              <input type="text" name="search" onChange={(e) =>{ setValue(e.target.value); }} />
-              <label>Filter by Region:</label>
-              <select onChange={(e) =>{ setRegion(e.target.value); }}>
-                <option value="not-selected"></option>
-                <option value="africa">Africa</option>
-                <option value="americas">Americas</option>
-                <option value="asia">Asia</option>
-                <option value="europe">Europe</option>
-                <option value="oceania">Oceania</option>
-              </select>
-              <input type="submit" value="Search"/>
+          <form
+            onSubmit={() => {
+              setSearched(true);
+            }}
+          >
+            <label>Search for Countries:</label>
+            <input
+              type="text"
+              name="search"
+              onChange={(e) => {
+                setValue(e.target.value);
+              }}
+            />
+            <label>Filter by Region:</label>
+            <select
+              onChange={(e) => {
+                setRegion(e.target.value);
+              }}
+            >
+              <option value="not-selected"></option>
+              <option value="africa">Africa</option>
+              <option value="americas">Americas</option>
+              <option value="asia">Asia</option>
+              <option value="europe">Europe</option>
+              <option value="oceania">Oceania</option>
+            </select>
+            <input type="submit" value="Search" />
           </form>
         </Container>
       </Navbar>
-      
-      
+
       <Router>
         <Routes>
-          <Route exact path="/" element={<Home/>}/>
-          <Route path="/country/:code" element={<Country/>}/>
-        </Routes>  
-    </Router>
-     
+          <Route exact path="/" element={<Home />} />
+          <Route path="/country/:code" element={<Country />} />
+        </Routes>
+      </Router>
     </div>
-    
   );
 }
 
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,58 +1,58 @@
 import React from "react";
 import { useSearchParams } from "react-router-dom";
-import 'bootstrap/dist/css/bootstrap.min.css';
-import CountryList from './CountryList';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import CountryList from "./CountryList";
 
 function Home() {
   const [data, setData] = React.useState(null);
   const [params, setParams] = useSearchParams();
   const search = params.get("search");
   const region = params.get("region");
-  
-  let api = "https://restcountries.com/v3.1/all"
 
-  if (search && search !== "null") 
-  {
+  let api = "https://restcountries.com/v3.1/all";
+
+  if (search && search !== "null") {
     api = "https://restcountries.com/v3.1/name/" + search;
-  } 
+  }
 
   const [status, setStatus] = React.useState(false);
 
   React.useEffect(() => {
-    document.title = "Country Viewer";
+    document.title = "Countries";
 
     fetch(api)
       .then((res) => res.json())
       .then((data) => {
-        console.log(api)
-        
+        console.log(api);
+
         if (Array.isArray(data) && region && region !== "null") {
           console.log("Filtering");
-          data = data.filter(d => d.region.toLowerCase() === region.toLowerCase());
+          data = data.filter(
+            (d) => d.region.toLowerCase() === region.toLowerCase()
+          );
         }
-        
-        
-        setStatus(data?.status); 
-        setData(data); 
-        console.log(data)});
-  }, []);
 
+        setStatus(data?.status);
+        setData(data);
+        console.log(data);
+      });
+  }, []);
 
   return (
     <div>
-      { 
-        status === 404 ? 
+      {status === 404 ? (
         <div className="center">
           <h1>
-              The country you requested does not exist! Please check your spelling.
+            The country you requested does not exist! Please check your
+            spelling.
           </h1>
-        </div> :
+        </div>
+      ) : (
         <div>
           <h1>Countries:</h1>
-          <CountryList data={ data }/>
+          <CountryList data={data} />
         </div>
-      }
+      )}
     </div>
   );
 }
